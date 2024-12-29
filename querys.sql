2edecd2d30df47ceed1a8b704c2f67fd49424f3b
Wilberth <stwilberth@gmail.com>
2024-12-29 00:00:01 -0600

    build

--- querys.sql
+++ querys.sql
@@ -1,4 +1,7 @@
 -- cambiar el tipo de dato de la columna oferta a enum
 ALTER TABLE productos MODIFY COLUMN oferta ENUM('0', '1', '2') DEFAULT '0';
 
+-- eliminar de la tabla productos las columnas nuevo, destacado, slider, fecha_inicio, fecha_fin, moneda, descuento, codigo
+ALTER TABLE productos DROP COLUMN nuevo, DROP COLUMN destacado, DROP COLUMN slider, DROP COLUMN fecha_inicio, DROP COLUMN fecha_fin, DROP COLUMN moneda, DROP COLUMN descuento, DROP COLUMN codigo;
+
 
