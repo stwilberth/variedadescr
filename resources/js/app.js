b014f0fbeb7ad6c4c9b0777b6921553b27634793
Wilberth <stwilberth@gmail.com>
2024-12-29 00:41:43 -0600

    ax

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,13 +5,6 @@ import 'bootstrap';
 import '../sass/app.scss';
 import { createApp } from 'vue';
 
-// Configuración de axios
-window.axios = axios;
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'; 
-
-// Configuración de jQuery
-window.$ = window.jQuery = jQuery;
-
 // Componentes Vue
 import ProductImageSlider from './components/ProductImageSlider.vue';
 import ProductsSlider from './components/ProductsSlider.vue';
